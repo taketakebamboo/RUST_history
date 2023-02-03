f0b7c8ba95294dde0d3392547da06e30b213bf68
taketakebamboo <takeuguisu@gmail.com>
2023-02-03 20:22:07 +0900

    [update] Win when break

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,28 +3,31 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!"); // 数字を当ててください
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); //行の読み込みに失敗しました
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
     let secret_number = rand::thread_rng().gen_range(1..101);
-
-    println!("The secret number is: {}", secret_number);
-    println!("Please input your guess."); //予想を入力してください
-
     loop {
+        println!("Guess the number!"); // 数字を当ててください
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); //行の読み込みに失敗しました
+
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+
+        println!("The secret number is: {}", secret_number);
+        println!("Please input your guess."); //予想を入力してください
+
         println!("You guessed: {}", guess); //次のように予想しました
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
