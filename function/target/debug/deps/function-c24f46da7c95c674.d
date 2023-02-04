7995c7c01b29bb46a35d5ea4b4bd24ed46dee016
taketakebamboo <takeuguisu@gmail.com>
2023-02-04 18:23:42 +0900

    [update] add function project create another_function

---
+++ function/target/debug/deps/function-c24f46da7c95c674.d
@@ -0,0 +1,5 @@
+/home/takeuguisu/rust/function/target/debug/deps/function-c24f46da7c95c674: src/main.rs
+
+/home/takeuguisu/rust/function/target/debug/deps/function-c24f46da7c95c674.d: src/main.rs
+
+src/main.rs:
---
+++ function/target/debug/function.d
@@ -0,0 +1,1 @@
+/home/takeuguisu/rust/function/target/debug/function: /home/takeuguisu/rust/function/src/main.rs
