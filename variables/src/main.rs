776394cfafa0edca27d35076971330deea2630b2
taketakebamboo <takeuguisu@gmail.com>
2023-02-04 17:09:06 +0900

    [update] variables array test input index

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,25 @@
+use std::io;
+
 fn main() {
-    let x = 5;
+    let a = [1, 2, 3, 4, 5];
 
-    let x = x + 1;
+    println!("Please enter an array index");
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-    println!("The value of x is: {}", x);
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .porse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
 }
