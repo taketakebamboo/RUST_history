49b96b1407dba0d502bb524c4f2a7f1898c867cd
taketakebamboo <takeuguisu@gmail.com>
2023-02-12 17:49:44 +0900

    [update] branches project for

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,10 +1,6 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
-
-    while index < 5 {
-        println!("The value is: {}", a[index]);
-
-        index += 1;
+    for number in (1..4).rev() {
+        println!("{}!", number);
     }
+    println!("LIFTOFF!!");
 }
