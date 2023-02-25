986f3b9270889685f184e86752fa5bd7f905cbe2
taketakebamboo <takeuguisu@gmail.com>
2023-02-26 00:15:38 +0900

    [understand] function back value ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,17 +1,13 @@
 fn main() {
-    let s1 = gives_ownership;
+    let s1 = String::from("hello");
 
-    let s2 = String::from("hello");
+    let (s2, len) = colculate_length(s1);
 
-    let s3 = take_and_gives_back(s2);
+    println!("The length of '{}' is {}.", s2, len);
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("hello!");
+fn colculate_length(s: String) -> (String, usize) {
+    let length = s.len();
 
-    some_string
-}
-
-fn take_and_gives_back(a_string: String) -> String {
-    a_string
+    (s, length)
 }
